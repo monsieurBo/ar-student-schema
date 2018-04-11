fd8f5ef7a8e2421a6b114526cdd00441791a58a9
monsieurBo <boscoho.hy@gmail.com>
2018-04-12 00:53:07 +0800

    Work Progress Night 1

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,5 +2,60 @@ require_relative '../../db/config'
 
 class Student < ActiveRecord::Base
 # implement your Student model here
+	include ActiveModel::Validations
+
+	attr_accessor :email, :birthday, :phone
+
+	validates :email, 
+			   		:presence => true, 
+			  		:uniqueness => { :case_sensitive => false },
+			  		:format => {with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]{2,}+\z/, message: 'is not a valid email address'}
+
+
+	validate :age_limit,
+						:min_phone_numbers
+
+	def name
+		fname = "#{self.first_name} #{self.last_name}"
+		return fname
+	end
+
+	def age
+		if Date.today.month > self.birthday.month 
+			age = Date.today.year - self.birthday.year
+		elsif Date.today.month < self.birthday.month || (Date.today.month == self.birthday.month && Date.today.day < self.birthday.day)
+			age = Date.today.year - self.birthday.year - 1
+		end
+		return age
+	end
+
+	private
+
+	def age_limit
+		if self.birthday.year >= Date.today.year - 5
+			errors.add(:birthday, 'must be at least 5 years old.')
+		end
+	end
+
+	def min_phone_numbers
+		numbers = self.phone
+		num = numbers.gsub(/[^\d]/, '')
+		res = num.length
+		if res < 10
+			errors.add(:phone, 'must contain at least 10 numbers')
+		end
+	end
 
 end
+
+# p Student.all
+# p student = Student.first
+# p student.name
+# p student.birthday.year
+# p student.birthday.month
+# p Time.now.month
+# p student.age
+
+# p student = Student.first
+# p student.phone
+# p student.min_phone_numbers
--- db/migrate/20121011144238_create_students.rb
+++ db/migrate/20121011144238_create_students.rb
@@ -6,9 +6,14 @@ class CreateStudents < ActiveRecord::Migration[5.0]
   def change
     # HINT: checkout ActiveRecord::Migration.create_table
     create_table :students do |t|
-
-      # add columns that you would need for this table
-
+    	t.string	:first_name
+    	t.string	:last_name
+    	t.string	:gender
+    	t.string	:email
+    	t.string	:phone
+    	t.date		:birthday
+    	t.datetime :created_at
+    	t.datetime :updated_at
       t.timestamps
     end
   end
